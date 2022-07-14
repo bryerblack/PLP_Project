655835eb88990c36fa6c7a5c8f6f86b5a9a3b4eb
francisandrade <francinhoandrade@gmail.com>
2022-07-14 14:33:30 -0300

    first

--- CorridaVelha.hs
+++ CorridaVelha.hs
@@ -1,4 +1,15 @@
-module CorridaVelha where
+module CorridaVelha (
+    startGame
+)
+where
+
+import Util
+    ( Cell(..),
+      CellTransform(..),
+      printBoard,
+      checkBoardFree,
+      verifyIsFree,
+      assignCell)
 
 startGame:: Int -> Int -> [Char] -> IO ()
 startGame b c d =  putStrLn "nao tem nada"
