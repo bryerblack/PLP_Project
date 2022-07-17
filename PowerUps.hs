69ecbc7122588c8afaea70563b9c95a195cd63d0
Diogo Silveira <diogoas132@gmail.com>
2022-07-16 23:02:51 -0300

    Adição de Lupin

--- PowerUps.hs
+++ PowerUps.hs
@@ -6,14 +6,21 @@ import Util
       Cell(..),
       CellTransform(..),
       printMsg,
-      transformeCell )
+      transformeCell,
+      checkPos)
 
-{-removeJogada:: Char -> [Cell] -> Int -> (Int, Int) -> [Cell]
-removeJogada syb board col dim = do
-  putStrLn "Digite a posição a remover: "
-  transformeCell Empty board col <$> getLine -}
+removeJogada:: (Int, Int) -> Char -> [Cell] -> (Int, Int) -> CellTransform
+removeJogada pos@(xPos, yPos) symbol board dim@(col, lin)=
+    if checkPos pos dim && verifyIsFree board col (xPos, yPos)
+        then Success (transformeCell (Empty) board col xPos yPos) (xPos, yPos)
+        else Fail board
 
 blip:: [Cell] -> (Int, Int) -> [Cell]
 blip board dim = do
     replicate (uncurry (*) dim) Empty
 
+lupin:: (Int, Int) -> Char -> [Cell] -> (Int, Int) -> CellTransform
+lupin pos@(xPos, yPos) symbol board dim@(col, lin)=
+  if checkPos pos dim && not (verifyIsFree board col (xPos, yPos))
+        then Success (transformeCell (Occupied symbol) board col xPos yPos) (xPos, yPos)
+        else Fail board
